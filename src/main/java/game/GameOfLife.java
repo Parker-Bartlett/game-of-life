014ba40a928d0c5b444482786aa5d5d902dab654
Parker-Bartlett <prbartlett1@gmail.com>
2019-03-29 18:26:46 -0400

    added user input and dynamic board size / generations

--- src/main/java/game/GameOfLife.java
+++ src/main/java/game/GameOfLife.java
@@ -1,13 +1,25 @@
 package game;
 
+import java.util.Scanner;
+
 public class GameOfLife {
 
 	public static void main(String[] args) {
-		Board board = new Board(8, 6);
+		Scanner input = new Scanner(System.in);
+		
+		System.out.println("Hello, how many columns would you like your board to have?");
+		int columns = input.nextInt();
+		System.out.println("Okay, how many rows?");
+		int rows = input.nextInt();
+		System.out.println("Great! How many generations would you like to see?");
+		int generations = input.nextInt();
+
+		
+		Board board = new Board(columns, rows);
 		board.generateNewBoard();
 		board.drawBoard();
 		
-		for(int i = 0; i < 100; i++) {			
+		for(int i = 0; i < generations; i++) {			
 			board.generateNextGenerationBoard();
 			board.drawBoard();
 		}
